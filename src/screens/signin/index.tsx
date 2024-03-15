65a6da80fc258523d2a90746442b5edf17616f84
Matheus Ramalho Silva <matheusramalhosilva2@gmail.com>
2024-03-15 16:46:25 -0300

    feat(main): gluestack nao ta funcionando direito

--- src/screens/signin/index.tsx
+++ src/screens/signin/index.tsx
@@ -1,12 +1,25 @@
-import { Box, VStack, Text } from '@gluestack-ui/themed'
+import { Box, VStack, Text, Center, Image } from '@gluestack-ui/themed'
+
+import LogoSvg from '@/assets/svgs/logo.svg'
+import BackgroundImg from '@/assets/imgs/background.png'
 
 export function Signin() {
     return (
-        <Box backgroundColor="$blue500" flex={1} px="$8" py="$2.5">
-            <VStack flex={1}>
-                <Text textTransform="capitalize" color="$pink300">
-                    algo nessa tela
-                </Text>
+        <Box backgroundColor="$gray700" flex={1} px="$8" py="$2.5">
+            <VStack flex={1} px={10} pb={16}>
+                <Image
+                    source={BackgroundImg}
+                    defaultSource={BackgroundImg}
+                    alt="Pessoas treinando"
+                    resizeMode="contain"
+                    position="absolute"
+                />
+
+                <Center my={24}>
+                    <LogoSvg />
+
+                    <Text fontSize="$sm">Treine sua mente e o seu corpo.</Text>
+                </Center>
             </VStack>
         </Box>
     )
